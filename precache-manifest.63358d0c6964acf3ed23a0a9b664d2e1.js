8d826bdbc6b352265f5911551ddf255d2ea78040
Myrat Charyyev <charyyev2018@mail.ru>
2021-10-13 10:56:49 +0500

    Updates

---
+++ precache-manifest.63358d0c6964acf3ed23a0a9b664d2e1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cb559b943456a51f6a0184bccb19404",
+    "url": "/barometric-weather-app%20/index.html"
+  },
+  {
+    "revision": "5cfc86a622f3ba5c797c",
+    "url": "/barometric-weather-app%20/static/css/2.ed439b8d.chunk.css"
+  },
+  {
+    "revision": "004ae06f51ccc182912c",
+    "url": "/barometric-weather-app%20/static/css/main.4e5c8fa9.chunk.css"
+  },
+  {
+    "revision": "5cfc86a622f3ba5c797c",
+    "url": "/barometric-weather-app%20/static/js/2.87855795.chunk.js"
+  },
+  {
+    "revision": "306f3bf9d2f1ee8399ad560a7731d0d6",
+    "url": "/barometric-weather-app%20/static/js/2.87855795.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "004ae06f51ccc182912c",
+    "url": "/barometric-weather-app%20/static/js/main.850cba2e.chunk.js"
+  },
+  {
+    "revision": "4440b6fcf67c28f71519",
+    "url": "/barometric-weather-app%20/static/js/runtime-main.51f3f8ed.js"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/barometric-weather-app%20/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/barometric-weather-app%20/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/barometric-weather-app%20/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/barometric-weather-app%20/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/barometric-weather-app%20/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
